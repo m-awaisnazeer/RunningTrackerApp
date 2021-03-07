d93f1b4d1907698c4dbc5ddad1635b8b46e293cb
Awais <m.awaisnaxeer@gmail.com>
2021-03-07 19:07:59 +0500

    Foreground Service is Created

--- app/src/main/java/com/communisolve/runningtrackerapp/services/TrackingService.kt
+++ app/src/main/java/com/communisolve/runningtrackerapp/services/TrackingService.kt
@@ -1,28 +1,87 @@
 package com.communisolve.runningtrackerapp.services
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.NotificationManager.IMPORTANCE_LOW
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_UPDATE_CURRENT
 import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
 import androidx.lifecycle.LifecycleService
+import com.communisolve.runningtrackerapp.Common.Common
 import com.communisolve.runningtrackerapp.Common.Common.ACTION_PAUSE_SERVICE
 import com.communisolve.runningtrackerapp.Common.Common.ACTION_START_OR_RESUME_SERVICE
 import com.communisolve.runningtrackerapp.Common.Common.ACTION_STOP_SERVICE
+import com.communisolve.runningtrackerapp.R
+import com.communisolve.runningtrackerapp.ui.MainActivity
 import timber.log.Timber
 
 class TrackingService : LifecycleService() {
 
+    var isFirstRun: Boolean = true
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         intent?.let {
-            when(it.action){
-                ACTION_START_OR_RESUME_SERVICE ->{
-                    Timber.d("Started or resumed service")
+            when (it.action) {
+
+                ACTION_START_OR_RESUME_SERVICE -> {
+                    if (isFirstRun) {
+                        startForeground()
+                        isFirstRun = false
+                    }else{
+                        Timber.d("resuming service")
+                    }
                 }
-                ACTION_PAUSE_SERVICE ->{
+                ACTION_PAUSE_SERVICE -> {
                     Timber.d("pause service")
                 }
-                ACTION_STOP_SERVICE ->{
+                ACTION_STOP_SERVICE -> {
                     Timber.d("Stopped service")
                 }
             }
         }
         return super.onStartCommand(intent, flags, startId)
     }
+
+    private fun startForeground() {
+        val notificationManager =
+            getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            createNotificationChannel(notificationManager)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(this, Common.NOTIFICATION_CHANNEL_ID)
+            .setAutoCancel(false)
+            .setOngoing(true)
+            .setSmallIcon(R.drawable.ic_directions_run_black_24dp)
+            .setContentTitle("Running App")
+            .setContentText("00:00:00")
+            .setContentIntent(getMainActivityPendingIntent())
+
+        startForeground(Common.NOTIFICATION_ID, notificationBuilder.build())
+
+    }
+
+    private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
+        this,
+        0,
+        Intent(this, MainActivity::class.java).also {
+            it.action = Common.ACTION_SHOW_TRACKING_FRAGMENT
+        },
+        FLAG_UPDATE_CURRENT
+    )
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel(notificationManager: NotificationManager) {
+        val channel = NotificationChannel(
+            Common.NOTIFICATION_CHANNEL_ID,
+            Common.NOTIFICATION_CHANNEL_Name,
+            IMPORTANCE_LOW
+        )
+        notificationManager.createNotificationChannel(channel)
+    }
 }
+
+
--- app/src/main/java/com/communisolve/runningtrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/communisolve/runningtrackerapp/ui/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.communisolve.runningtrackerapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.communisolve.runningtrackerapp.Common.Common
 import com.communisolve.runningtrackerapp.Database.RunDao
 import com.communisolve.runningtrackerapp.R
 import dagger.hilt.android.AndroidEntryPoint
@@ -22,6 +24,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
 
+        navigateToTrackingFragmentIfNeeded(intent)
+
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
 
         navHostFragment.findNavController()
@@ -34,6 +38,17 @@ class MainActivity : AppCompatActivity() {
                 }
                 }
             }
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        navigateToTrackingFragmentIfNeeded(intent)
+    }
+
+    private fun navigateToTrackingFragmentIfNeeded(intent:Intent?){
+        if (intent?.action == Common.ACTION_SHOW_TRACKING_FRAGMENT){
+            navHostFragment.findNavController().navigate(R.id.action_global_trackingFragment)
+        }
     }
 
 
