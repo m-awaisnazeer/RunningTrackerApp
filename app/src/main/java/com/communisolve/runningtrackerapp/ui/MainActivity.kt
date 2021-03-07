61018a407a3eeccf7d70104f8cb03157eb636cfa
Awais <m.awaisnaxeer@gmail.com>
2021-03-07 10:01:53 +0500

    Navigations of Fragments Added

--- app/src/main/java/com/communisolve/runningtrackerapp/ui/MainActivity.kt
+++ app/src/main/java/com/communisolve/runningtrackerapp/ui/MainActivity.kt
@@ -3,20 +3,38 @@ package com.communisolve.runningtrackerapp.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.communisolve.runningtrackerapp.Database.RunDao
 import com.communisolve.runningtrackerapp.R
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.activity_main.*
 import javax.inject.Inject
 
 private const val TAG = "MainActivity"
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    @Inject
-    lateinit var runDao: RunDao
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "onCreate:${runDao.hashCode()} ")
+        setSupportActionBar(toolbar)
+
+        bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { controller, destination, arguments ->
+                when(destination.id){
+                    R.id.settingsFragment,R.id.statisticsFragment, R.id.runFragment ->{
+                        bottomNavigationView.visibility = View.VISIBLE
+                    }else ->{
+                    bottomNavigationView.visibility = View.GONE
+                }
+                }
+            }
     }
+
+
 }
--- app/src/main/java/com/communisolve/runningtrackerapp/ui/fragments/SetUpFragment.kt
+++ app/src/main/java/com/communisolve/runningtrackerapp/ui/fragments/SetUpFragment.kt
@@ -1,7 +1,20 @@
 package com.communisolve.runningtrackerapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.communisolve.runningtrackerapp.R
+import kotlinx.android.synthetic.main.fragment_setup.*
 
 class SetUpFragment:Fragment(R.layout.fragment_setup) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        tvContinue.setOnClickListener {
+            findNavController().navigate(R.id.action_setUpFragment_to_runFragment)
+        }
+    }
 }
